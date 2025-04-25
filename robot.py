5f361497a15fdbaceb8f2f68d3a5ea9270188c95
hi980whatsup <142428966+hi980whatsup@users.noreply.github.com>
2025-04-25 22:19:27 +0545

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,14 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        print("Hello, I am " + self.name + "!")
+
+
+robot1 = Robot("Tom")
+robot2 = Robot("Jerry")
+
+
+robot1.introduce()
+robot2.introduce()
